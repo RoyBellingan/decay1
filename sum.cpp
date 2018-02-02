c402c297e2c095756a3ed412db1a706d63171766
Roy Bellingan <admin@seisho.us>
2018-02-02 23:39:49 +0000

    ok

--- sum.cpp
+++ sum.cpp
@@ -64,16 +64,7 @@ __kernel void add_numbers(__global uint* iteration,__global uint* nuclide , __gl
 		float v = rand(&state,g1);
 		if(v  < prob->x){// compare r with probability density function, here p = const.: is there a decay?
 			atom_dec(nuclide);
-			//BONUS! check if we decay immediately ^_^ no fence, no memory reordering no nothing!!!
-			v = rand(&state,g1);
-			if(v < prob->y){ //compare r with probability density function, here p = const.: is there a decay?
-				//atom_dec(nuclide + 1);
-				//DO NOT DO NOTHING AHAHHA!
-			}else{
-				//ok we gained one
-				atom_inc(nuclide + 1);
-			}
-
+			atom_inc(nuclide + 1);
 		}
 	}
 
@@ -82,12 +73,12 @@ __kernel void add_numbers(__global uint* iteration,__global uint* nuclide , __gl
 	if(g1 == (24 * 192) - 1){//LAST kernel run the remnant and do the swap!
 		atomic_xchg((nuclide + 2),*(nuclide + 1));
 		max = atomic_max((nuclide + 2),0) % ((24 * 192) - 1);
-		//printf("k: %u - iter %u swapped %u  old %u\n",g1, max, atomic_max((nuclide + 2),0), *(nuclide + 1));
+		printf("k: %u - iter %u swapped %u  old %u\n",g1, max, atomic_max((nuclide + 2),0), *(nuclide + 1));
 	}
 	//all the thread will wait here that the last perform the swap and than restart
 	barrier(CLK_GLOBAL_MEM_FENCE);
 
-	if(g1 != (24 * 192) - 1){//LAST kernel run the remnant and do the swap!
+	if(g1 != (24 * 192) - 1){
 		max = atomic_max((nuclide + 2),0) / ((24 * 192) - 1);
 	}
 
@@ -95,34 +86,14 @@ __kernel void add_numbers(__global uint* iteration,__global uint* nuclide , __gl
 	if(g1 == 1){//avoid spamming
 		//printf("k: %u - iter %u swapped %u  old %u\n",g1, max, atomic_max((nuclide + 2),0), *(nuclide + 1));
 	}
-//		atomic_xchg(nuclide + 4,0);
-//		atomic_xchg(nuclide + 3,0);
-//atomic_xchg(nuclide + 5,0);
-//atomic_xchg(nuclide + 6,0);
-	barrier(CLK_GLOBAL_MEM_FENCE);
 
-
-
-	//Surviving Radon LOOP
+	//Radon LOOP
 	for(ulong j = 0;
 	    j < max;
 	    j++) {
-//		atomic_inc(nuclide + 4);
-//		atomic_inc(nuclide + 3);
-//		atomic_inc(nuclide + 5);
-//		atomic_inc(nuclide + 6);
-
 		float v = rand(&state,g1);
 		if(v < prob->y){ //compare r with probability density function, here p = const.: is there a decay?
 			atom_dec(nuclide + 1);
-		}else{
-//			printf("not boom %f? \n", v);
 		}
 	}
-
-//	barrier(CLK_GLOBAL_MEM_FENCE);
-//	printf("total run: %u \n", atomic_max((nuclide + 4),0));
-//	printf("total run: %u \n", atomic_max((nuclide + 3),0));
-//	printf("total run: %u \n", atomic_max((nuclide + 5),0));
-//	printf("total run: %u \n", atomic_max((nuclide + 6),0));
 }
